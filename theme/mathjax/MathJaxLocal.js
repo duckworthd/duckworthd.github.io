44e5004c2c427af7b5be70db0441026814d5ce50
Daniel Duckworth <duckworthd@gmail.com>
2021-08-08 13:05:22 +0300

    responsive design; remove code page

--- theme/mathjax/MathJaxLocal.js
+++ theme/mathjax/MathJaxLocal.js
@@ -1,28 +1,14 @@
 // configure mathjax
-MathJax.Hub.Config({
-	jax: ["input/TeX","output/HTML-CSS"],
-  extensions: ["tex2jax.js","MathMenu.js","MathZoom.js"],
-  tex2jax:
-		{
-			inlineMath     : [ ['$','$']  , ['\\(','\\)'] ],
-			displayMath    : [ ['$$','$$'], ['\\[','\\]'] ],
-			processEscapes : true
-		},
-	TeX:
-		{
-			equationNumbers: { autoNumber: "AMS" },
-			TagSide: "left",
-      Macros: {
-        norm: ['\\left\\lVert#1\\right\\rVert', 1],
-				argmin: ['\\mathop{\\mathrm{arg\\,min}}'],
-				argmax: ['\\mathop{\\mathrm{arg}\\,\\max}']
-      }
-		},
-	"HTML-CSS": {
-    scale          : 100,
-    availableFonts : ["TeX"],
-  }
-});
-
-// notify other scripts that this file has finished loading
-MathJax.Ajax.loadComplete("MathJaxLocal.js");
+window.MathJax = {
+  jax: ["input/TeX","output/HTML-CSS"],
+  tex: {
+    inlineMath     : [ ['$','$']  , ['\\(','\\)'] ],
+    displayMath    : [ ['$$','$$'], ['\\[','\\]'] ],
+    macros: {
+      norm: ['\\left\\lVert#1\\right\\rVert', 1],
+      argmin: ['\\mathop{\\mathrm{arg\\,min}}'],
+      argmax: ['\\mathop{\\mathrm{arg}\\,\\max}']
+    },
+    processEscapes : true
+  },
+};
